971aea066613222610e1e05c1674f55c728d0291
thomasjthomasj <thomasjmarchant@hotmail.com>
2013-08-25 18:33:43 +0100

    set up basic structure for main game file

---
+++ main.py
@@ -0,0 +1,22 @@
+# Drew Krime: Streets of Ellay
+
+project_title = "Drew Krime: Streets of Ellay"
+
+# Import dependencies
+import pygame, sys, os
+
+# Check if Windows and import pygame._view if so
+from sys import platform as _platform
+if _platform == "win32" or _platform == "cygwin":
+    import pygame._view
+
+# Initialise pygame library
+pygame.init()
+
+# Import game files
+
+# Setup screen
+
+# Run game
+
+# Program loop

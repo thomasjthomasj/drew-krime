6bca9ef03606044ff3ba2711fda37bc143f1f088
thomasjthomasj <thomasjmarchant@hotmail.com>
2013-08-26 15:12:42 +0100

    set sprite path variable

--- game/visuals/Sprite.py
+++ game/visuals/Sprite.py
@@ -2,9 +2,11 @@ import pygame
 
 class Sprite(pygame.sprite.Sprite):
     
+    sprite_path = 'sprites/'
+    
     def __init__(self, src, pos):
         super(Sprite, self).__init__()
-        self.src_image = pygame.image.load(src).convert_alpha()
+        self.src_image = pygame.image.load(sprite_path + src).convert_alpha()
         self.src_width, self.src_height = self.src_image.get_size()
         x_pos = pos[0] - self.src_width / 2
         y_pos = pos[1] - self.src_height / 2

b705fc25f7d8152c4dc0e2386d0ffe31c6d8a22d
thomasjthomasj <thomasjmarchant@hotmail.com>
2013-09-07 20:19:02 +0100

    removed .view thing

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import pygame
 
 # Check if Windows and import pygame._view if so
 from sys import platform as _platform
-if _platform == "win32" or _platform == "cygwin":
-    import pygame._view
+#if _platform == "win32" or _platform == "cygwin":
+#    import pygame._view
 
 # Initialise pygame library
 pygame.init()

c277766559e7018ea096d810927871a68582ea42
thomasjthomasj <thomasjmarchant@hotmail.com>
2013-09-08 21:44:34 +0100

    on platform detection from platform class working

--- game/world/Platform.py
+++ game/world/Platform.py
@@ -1,6 +1,7 @@
 import pygame
 
 from game.world.Terrain import Terrain
+from game.Game import Game
 
 class Platform(Terrain):
     
@@ -9,11 +10,22 @@ class Platform(Terrain):
     def __init__(self, pos, src, width, height):
         super(Platform, self).__init__(pos, src, width, height)
     
-    def onPlatform(self, player):
+    def onPlatform(self, character):
         if self.top_clip == False:
             return False
-        if player.foot_pos <= self.pos[1] and player.foot_pos > self.pos[1] - player.vel_y:
+        if self.onTop(character) and self.inBoundry(character):
             return True
-        
         return False
     
+    def inBoundry(self, character):
+        return self.inLeftBoundry(character) and self.inRightBoundry(character)
+    
+    def inLeftBoundry(self, character):
+        return character.pos[0] - character.src_width > self.pos[0]
+    
+    def inRightBoundry(self, character):
+        return character.pos[0] < self.pos[0] + self.width
+    
+    def onTop(self, character):
+        boundry = self.pos[1] - character.move_y
+        return character.foot_pos <= self.pos[1] and character.foot_pos > boundry
